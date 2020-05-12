d7430b4448b3ad29c92aa70d0865603a95d1de61
TheFluyter <rick__ruiter@hotmail.com>
2020-05-12 20:07:30 +0200

    Created first activity with search bar and enter button and an empty second activity

--- app/src/main/java/edu/harvard/cs50/mtgcards/MainActivity.java
+++ app/src/main/java/edu/harvard/cs50/mtgcards/MainActivity.java
@@ -1,14 +1,26 @@
 package edu.harvard.cs50.mtgcards;
 
 import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    public static final String USER_SEARCH = "edu.harvard.cs50.mtgcards.USER_SEARCH";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void searchCard(View v) {
+        Intent intent = new Intent(this, SearchAdapter.class);
+        EditText editText = findViewById(R.id.search_bar);
+        String search = editText.getText().toString();
+        intent.putExtra(USER_SEARCH, search);
+        startActivity(intent);
     }
 }
---
+++ app/src/main/java/edu/harvard/cs50/mtgcards/SearchAdapter.java
@@ -0,0 +1,14 @@
+package edu.harvard.cs50.mtgcards;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SearchAdapter extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_search_adapter);
+    }
+}
